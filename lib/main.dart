e255ee40ba3e4bd408cb74d752198ea455a0a9fd
Ahmadqrs <saboorquraishe@gmail.com>
2026-09-08 06:16:59 +0430

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'services/database_service.dart';
+import 'screens/home_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await DatabaseService.db; // دیتابیس رو اول لود کن
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'توشه آخرت',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
